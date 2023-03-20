a531bbffdbbbb32fd42b2b6f024c56f5a1a083dc
Ibrahim Hassan <ibrahim.hassan2402@gmail.com>
2023-03-20 17:27:23 +0000

    course_list template v1.0

--- MyCurriculum/views/courses.py
+++ MyCurriculum/views/courses.py
@@ -4,6 +4,5 @@ from ..models import Course
 
 
 def courses_list_view(request):
-    context_dict = {}
-    context_dict['course_list'] = Course.objects
+    context_dict = {'course_list': Course.objects}
     return render(request, 'courses/course_list.html', context=context_dict)

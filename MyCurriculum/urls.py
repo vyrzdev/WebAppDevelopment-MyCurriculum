9b9ef37d5c6e18fefef927036d41306b06a62af8
Benjamin Parsons-Willis <ben@vyrz.dev>
2023-03-20 17:02:53 +0000

    Sprint - Enrollment management. - Enroll in course - Unroll in course - List Enrollments in courses

--- MyCurriculum/urls.py
+++ MyCurriculum/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path("auth/register", views.auth.register_view, name='register-view'),
     path("auth/login", views.auth.login_view, name='login-view'),
     path("auth/logout", views.auth.logout_view, name='logout-view'),
-    path("enroll/<str:course_code>", views.enrollment.enroll_view, name='enroll-view')
+    path("enroll/<str:course_code>", views.enrollment.enroll_view, name='enroll-view'),
+    path("unroll/<str:course_code>", views.enrollment.unroll_view, name='unroll-view'),
+    path("enrollments", views.enrollment.enroll_summary_view, name='enroll-summary-view')
 ]
--- MyCurriculum/views/enrollment.py
+++ MyCurriculum/views/enrollment.py
@@ -1,13 +1,17 @@
 from django.http import HttpRequest, HttpResponse, Http404
+from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_http_methods
+
 from ..models import Course, UserCourseEnrollment
 
 
+@require_http_methods(['GET'])
 @login_required
 def enroll_view(request: HttpRequest, course_code: str):
     course_query = Course.objects.filter(pk=course_code)
     if not course_query.exists():
-        return Http404("No course with that ID!")
+        raise Http404("No course with that ID!")
 
     course = course_query.first()
     user = request.user
@@ -26,3 +30,38 @@ def enroll_view(request: HttpRequest, course_code: str):
     new_enrollment.save()
     return HttpResponse("Enrollment Created!")
 
+@require_http_methods(['GET'])
+@login_required
+def unroll_view(request: HttpRequest, course_code: str):
+    course_query = Course.objects.filter(pk=course_code)
+    if not course_query.exists():
+        raise Http404("No course with that ID!")
+
+    course = course_query.first()
+    user = request.user
+
+    enrollment_query = UserCourseEnrollment.objects.filter(
+        course=course,
+        user=user
+    )
+    if not enrollment_query.exists():
+        return HttpResponse("You arent enrolled!")
+
+    enrollment = enrollment_query.first()
+    enrollment.delete()
+    return HttpResponse("Enrollment Deleted!")
+
+@require_http_methods(['GET'])
+@login_required
+def enroll_summary_view(request: HttpRequest):
+    user = request.user
+    enrollment_query = UserCourseEnrollment.objects.filter(
+        user=user
+    )
+    return render(
+        request,
+        'enrollment/summary.html',
+        context={
+            'enrollments': enrollment_query.all()
+        }
+    )

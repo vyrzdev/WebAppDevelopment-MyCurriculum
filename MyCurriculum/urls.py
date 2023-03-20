fc38610da5d41ca0cdb32edee8ecc00cc658f400
Ibrahim Hassan <ibrahim.hassan2402@gmail.com>
2023-03-20 17:03:10 +0000

    course list view v1.0

--- MyCurriculum/urls.py
+++ MyCurriculum/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("auth/register", views.auth.register_view, name='register-view'),
     path("auth/login", views.auth.login_view, name='login-view'),
     path("auth/logout", views.auth.logout_view, name='logout-view'),
-    path("enroll/<str:course_code>", views.enrollment.enroll_view, name='enroll-view')
+    path("enroll/<str:course_code>", views.enrollment.enroll_view, name='enroll-view'),
+    path("courses/course-list", views.courses.course_list_view, name="course-list-view"),
 ]
--- MyCurriculum/views/courses.py
+++ MyCurriculum/views/courses.py
@@ -1,17 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from MyCurriculum.models import course
+from ..models import Course
 
-def login(request):
-    category_list = Category.objects.order_by('-likes')[:5]
-    pages_list = Page.objects.order_by('-views')[:5]
 
+def courses_list_view(request):
     context_dict = {}
-    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
-    context_dict['categories'] = category_list
-    context_dict['pages'] = pages_list
-    context_dict['extra'] = 'From the model solution on GitHub'
-
-    visitor_cookie_handler(request)
-
-    return render(request, 'rango/index.html', context=context_dict)
+    context_dict['course_list'] = Course.objects
+    return render(request, 'courses/course_list.html', context=context_dict)

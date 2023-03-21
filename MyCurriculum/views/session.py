2595a1fedf39a1ea375a21e75a27b64907201230
Benjamin Parsons-Willis <ben@vyrz.dev>
2023-03-21 13:28:46 +0000

    Started course-session views.

--- MyCurriculum/views/session.py
+++ MyCurriculum/views/session.py
@@ -1,10 +1,19 @@
+from django.shortcuts import render
 from django.http import HttpRequest, Http404
 from django.views.decorators.http import require_http_methods
 from ..models import CourseSession
 # TODO: Session management//views
 
+
 @require_http_methods(['GET'])
 def session_view(request: HttpRequest, session_id: str):
-    session_query = CourseSession.objects.filter(pk=None)
+    session_query = CourseSession.objects.filter(pk=session_id)
     if not session_query.exists():
         raise Http404("No session with that ID!")
+    return render(
+        request,
+        'sessions/view.html',
+        context={
+            'session': session_query.first()
+        }
+    )

465f5f193d6ee0a55673dd438c68efca47bcfaa2
Benjamin Parsons-Willis <ben@vyrz.dev>
2023-03-22 18:04:23 +0000

    Reorganized, took steps towards moderation view

--- MyCurriculum/views/moderation.py
+++ MyCurriculum/views/moderation.py
@@ -4,12 +4,13 @@ from django.shortcuts import render
 from django.contrib.auth import get_user_model
 
 from ..forms.moderation import CreateCourseForm
-from ..models import User
+from ..models import User, Course
 
 from typing import Callable
 
 User: User = get_user_model()  # Get user model, and do some sneaky typing magic for autocomplete :)
 
+PER_PAGE = 20
 
 # Decorator that requires django superuser level.
 def require_admin(func: Callable) -> Callable:
@@ -37,11 +38,22 @@ def moderation_index_view(request: HttpRequest):
 @require_http_methods(["GET"])
 @require_admin
 def moderation_manage_courses_view(request: HttpRequest):
+    # Get page number
+    try:
+        page_number = int(request.GET.get('page', default=0))
+    except ValueError:
+        page_number = 0
+
+    # Build queryset.
+    courses = Course.objects.all()[(page_number*PER_PAGE):((page_number*PER_PAGE)+PER_PAGE)]
+
+
     create_course_form = CreateCourseForm()
     return render(
         request,
         'moderation/courses.html',
         context={
-            'create_course_form': create_course_form
+            'create_course_form': create_course_form,
+            'courses': courses
         }
     )
